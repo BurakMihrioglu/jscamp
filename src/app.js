18538d9975119b3506d0705b105d69e8adb5d5fa
BurakMihrioglu <77462692+BurakMihrioglu@users.noreply.github.com>
2021-10-19 00:02:51 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("Merhaba Kodlama.io")
+
+// JS type safe degildir 
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun= 11.2
+//euroDun = 11 bunu yapamıyoruz
+
+console.log(euroDun)
+
+//array, çoğul isimlerdir , camelCasing'dir (konutKredileri), PascalCasing(Konut Kredisi)
+let konutKredileri = ["Konut Kredisi","Emalk Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+console.log(konutKredileri)
+console.log("</ul>")
